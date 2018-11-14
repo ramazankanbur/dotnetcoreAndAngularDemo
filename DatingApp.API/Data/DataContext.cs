55f9cc71e26603748f9d5979ddc60e2c30c477b7
ramazan kanbur <ramazanknbr@gmail.com>
2018-11-14 20:13:45 +0300

    initial commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,27 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        private IConfiguration _config;
+        public DataContext(IConfiguration config, DbContextOptions<DataContext> options) : base(options)
+        {
+            this._config = config;
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            //_config.GetConnectionString("DefaultConnection"); buda kullanılabilir
+            //dev db için appsettings.development.json içine aynı key ile db eklenirse o çalışır
+            optionsBuilder.UseSqlServer(_config["ConnectionStrings:DefaultConnection"]);
+        }
+
+
+    }
+}
---
+++ DatingApp.API/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API
+{
+    public class Program
+    {
+        //entry point
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+        //use Kestrel as the web server and configure it using the application's configuration
+        //Web sunucusu olarak lightweight bir kod kullanılıyor. (IIS yada apache kadar kuvvetli değil)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+    }
+}
